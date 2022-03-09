78b21e7dee30c9a74cd9d60a9df11ac587c4bac7
nijason7789 <root@Casval-M1.local>
2022-03-09 17:36:53 +0800

    Finished var practices

--- Practice1/FirstDart.dart
+++ Practice1/FirstDart.dart
@@ -1,3 +1,36 @@
-void main(){
+int getResult() {
+  return 8818;
+}
+
+void main() {
   print('HELLO world?');
+  var i = 87;
+  var j = 54;
+  var k = i + j;
+  print('i = $i');
+  print('$i + $j = $k');
+  print(getResult());
+
+  var x = (i > j);
+  print(x);
+
+  String s1 = "Risk of Rain2";
+  var ROR2 = getResult();
+  print('$s1, $ROR2');
+
+  //字串轉整數 (string to int)
+  var intVal = int.parse('100');
+  print(intVal);
+
+  //字串轉浮點數 (string to double)
+  var doubleVal = double.parse('99.9');
+  print(doubleVal);
+
+  //整數轉字串 (int to string)
+  String strVal1 = 66.toString();
+  print(strVal1);
+
+  //浮點數點轉字串，取道小數點後兩位 (double to string)
+  String strVal2 = 3.14159.toStringAsFixed(2);
+  print(strVal2);
 }
